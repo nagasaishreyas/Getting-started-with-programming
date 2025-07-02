5efdec902ed86209af13a3dc761a66597abb586d
sudheerkuntumalla <sudheerkuntumalla3004@gmail.com>
2025-07-02 20:39:24 +0530

    keyword

---
+++ main.py
@@ -0,0 +1,26 @@
+print("welcome to the world of programming! ")
+print(4)
+print("Hello python \n")
+print("Hello", 5)
+print("Welcome to", end= "*")
+
+
+x = 8
+y = "Shreyas"
+
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+name = input("entre your name: ")
+print("\nHello", name ,"\nWelome to codingal")
+
+
+
+
+
+import keyword
+
+print("Python keywords are:  \n")
+print(keyword.kwlist)
